0f032ee3638d39d2c556b2fefb6167a7368b85a2
chenhu <9517530789Hu>
2023-11-28 13:30:59 +0800

    first commit

---
+++ src/main/java/com/server/ServerArrangeApplication.java
@@ -0,0 +1,13 @@
+package com.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerArrangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerArrangeApplication.class, args);
+    }
+
+}
